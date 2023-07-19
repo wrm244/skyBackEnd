784bbf91c801905cc216304a0794e37cfdf4378c
wrm244 <wrm244@outlook.com>
2023-07-19 18:48:14 +0800

    fix: 添加跨域修复与修改数据库配置

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -10,10 +10,7 @@ import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,6 +20,7 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/admin/employee")
+@CrossOrigin(origins ={"*"} )
 @Slf4j
 public class EmployeeController {
 
