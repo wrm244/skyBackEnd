de72bfd4f2c8677b61118c76b678ad7b804cda11
wrm244 <wrm244@outlook.com>
2023-07-20 15:53:28 +0800

    feat: 启用禁用员工账号功能

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -107,4 +107,20 @@ public class EmployeeServiceImpl implements EmployeeService {
         return new PageResult(total,records);
     }
 
+    
+    @Override
+    public void startOrStop(Integer status, Long id) {
+        // Employee employee = new Employee();
+        // employee.setStatus(status);
+        // employee.setId(id);
+
+        Employee employee = Employee.builder() //builder的原理是在代码内部实现build内部静态类
+                        .id(id)
+                        .status(status)
+                        .build();
+        employeeMapper.update(employee);
+        
+    }
+
+    
 }
