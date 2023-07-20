93ea9c602aa44717abab7eb58062951874b5ca6a
wrm244 <wrm244@outlook.com>
2023-07-20 16:27:03 +0800

    feat: 编辑员工代码开发

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -122,5 +122,19 @@ public class EmployeeServiceImpl implements EmployeeService {
         
     }
 
-    
+    @Override
+    public Employee getById(Integer id) {
+        Employee employee = employeeMapper.getById(id);
+        employee.setPassword("****");
+        return employee;
+    }
+
+    @Override
+    public void update(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeDTO, employee);
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(BaseContext.getCurrentId());
+        employeeMapper.update(employee); 
+    }
 }
